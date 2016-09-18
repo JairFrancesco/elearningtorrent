544d650756ef3ca16f9be15ced9e7bf498e3b88d
Jair Francesco <jairfrancesco@gmail.com>
2016-09-18 03:23:08 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var currentUrl = window.location;
 var socket = io.connect('https://elearningp2p.ml:5000');
   socket.on('torrents', function (data) {
   console.log(data);
-  for (var i = 0; i<=data.length;i++)
+  for (var i = 0; i<data.length;i++)
   {
 	console.log(data[i]);
 	$('#torrents').append($('<li>').append('<a href="#output" class="torrentLinks">' + currentUrl + 'torrents/' + data[i] + '</a>'));
@@ -37,50 +37,50 @@ var $uploadSpeed = document.querySelector('#uploadSpeed')
 var $downloadSpeed = document.querySelector('#downloadSpeed')
 
 function downloadSelectedTorrent(torrentId){
-console.log(torrentId);
+  console.log(torrentId);
 
-currentTorrent = torrentId;
-// Download the torrent
-client.add(torrentId, function (torrent) {
+  currentTorrent = torrentId;
+  // Download the torrent
+  client.add(torrentId, function (torrent) {
 
-  // Stream the file in the browser
-  torrent.files[0].renderTo('#videoPlayer')
+    // Stream the file in the browser
+    torrent.files[0].renderTo('#videoPlayer')
 
-  // Trigger statistics refresh
-  //torrent.on('done', onDone)
-  //setInterval(onProgress, 500)
-  //onProgress()
-  
-  // Statistics
-  function onProgress () {
-    // Peers
-    $numPeers.innerHTML = torrent.numPeers + (torrent.numPeers === 1 ? ' peer' : ' peers')
-
-    // Progress
-    var percent = Math.round(torrent.progress * 100 * 100) / 100
-    $progressBar.style.width = percent + '%'
-    $downloaded.innerHTML = prettyBytes(torrent.downloaded)
-    $total.innerHTML = prettyBytes(torrent.length)
-
-    // Remaining time
-    var remaining
-    if (torrent.done) {
-      remaining = 'Done.'
-    } else {
-      remaining = moment.duration(torrent.timeRemaining / 1000, 'seconds').humanize()
-      remaining = remaining[0].toUpperCase() + remaining.substring(1) + ' remaining.'
-    }
-    $remaining.innerHTML = remaining
+    // Trigger statistics refresh
+    //torrent.on('done', onDone)
+    //setInterval(onProgress, 500)
+    //onProgress()
     
-    // Speed rates
-    $downloadSpeed.innerHTML = prettyBytes(torrent.downloadSpeed) + '/s'
-    $uploadSpeed.innerHTML = prettyBytes(torrent.uploadSpeed) + '/s'
-  }
-  function onDone () {
-    $body.className += ' is-seed'
-    onProgress()
-  }
-})
+    // Statistics
+    function onProgress () {
+      // Peers
+      $numPeers.innerHTML = torrent.numPeers + (torrent.numPeers === 1 ? ' peer' : ' peers')
+
+      // Progress
+      var percent = Math.round(torrent.progress * 100 * 100) / 100
+      $progressBar.style.width = percent + '%'
+      $downloaded.innerHTML = prettyBytes(torrent.downloaded)
+      $total.innerHTML = prettyBytes(torrent.length)
+
+      // Remaining time
+      var remaining
+      if (torrent.done) {
+        remaining = 'Done.'
+      } else {
+        remaining = moment.duration(torrent.timeRemaining / 1000, 'seconds').humanize()
+        remaining = remaining[0].toUpperCase() + remaining.substring(1) + ' remaining.'
+      }
+      $remaining.innerHTML = remaining
+      
+      // Speed rates
+      $downloadSpeed.innerHTML = prettyBytes(torrent.downloadSpeed) + '/s'
+      $uploadSpeed.innerHTML = prettyBytes(torrent.uploadSpeed) + '/s'
+    }
+    function onDone () {
+      $body.className += ' is-seed'
+      onProgress()
+    }
+  })
 };
 
 // Human readable bytes util

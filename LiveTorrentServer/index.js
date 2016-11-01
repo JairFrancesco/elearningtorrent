6840294bc5065c7cd200af16dcdb897a6714d1fd
Jair Francesco Huaman Canqui <jairfrancesco@gmail.com>
2016-11-01 01:51:44 -0500

    Torrent generates

--- LiveTorrentServer/index.js
+++ LiveTorrentServer/index.js
@@ -1,65 +1,49 @@
-//Programming the server side for the generate secuence of torrents file
-//Program the Socket server for emitting info about the segment video for playing
-
-/* Steps
-
-1.-  Generate .ts files with RTMP Module (NGINX) (Complete).
-2.-  Convert .ts to mp4 file (Callback is a function that receive the filename and convert it to dash).
-3.-  After the dash convert is completed the callback receive the filename of the dash file.
-4.-  Generate torrent file for the dash video file.
-5.-  Send the torrent link via Socket.io to the clients.
-6.-  
-
-
-Socket.io (Server)
-1.- On client connection send the last torrent link generated.
-2.- (Server) on torrent generated emit newSegment(torrentLink).
-3.- 
-
-Socket.io (Client)
-1.- On Connection receive the last torrent and stream it to html5 video tag
-2.- 
-
-
-Idea for managing problems on clients
- .-Have a chunk number 
-1.- Create  a manifest or something that has a list of torrents
-
-
-*/
-
 //Require necessary modules.
 
 var http = require("http");
 var hound = require('hound');
+var express = require('express');
+var app = express();
+var path = require('path');
+var https = require('https');
+var formidable = require('formidable');
+var fs = require('fs');
+var exec = require('child_process').exec;
 
 //General Variables
 
 var PORT = 7000;
 var TS_CHUNKS_DIRECTORY = '/HLS/live';
 var URL_CHUNKS = 'https://elearningp2p.ml:4430/live/';
+var lastTorrent;
 
-http.createServer(function (request, response) {
-   response.writeHead(200, {'Content-Type': 'text/plain'});
-   // Send the response body as
-   response.end('Hello World, LiveTorrentServer Working\n');
-}).listen(PORT);
+var server = https.createServer({
+      key: fs.readFileSync('/etc/letsencrypt/live/elearningp2p.ml/privkey.pem'),
+      cert: fs.readFileSync('/etc/letsencrypt/live/elearningp2p.ml/fullchain.pem')
+    }, app).listen(PORT);
 
-// Console will print the message
-console.log('Server running at http://127.0.0.1:7000/');
+var io = require('socket.io')(server);
 
+app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+
+app.get('/', function(req, res){
+  res.send("Server For Live Streaming using webtorrent");
+});
+
+console.log('Server running at http://127.0.0.1:'+PORT);
 
 // Create a directory tree watcher.
 watcher = hound.watch(TS_CHUNKS_DIRECTORY)
 
-// Create a file watcher.
-//watcher = hound.watch('/tmp/file.txt')
-
-var exec = require('child_process').exec;
-
-// Add callbacks for file and directory events.  The change event only applies
-// to files.
+//Create the torrents
 watcher.on('create', function(file, stats) {
   console.log(file + ' was created')
   var tmp = file.split("/");
@@ -67,9 +51,6 @@ watcher.on('create', function(file, stats) {
   console.log("Nombre del archivo: " + filename);
   var filenameWithoutExt = filename.split(".")[0];
   console.log("Sin extension:" + filenameWithoutExt);
-
-  //Para crear torrents
-  //create-torrent --urlList 'https://webseed.btorrent.xyz/timedrift-alpine-4k-timelapse.mp4' timedrift-alpine-4k-timelapse.mp4 > timedrift.torrent
 
   var tmpStreamNumber = filenameWithoutExt.split('-');
   var streamName = tmpStreamNumber[0];
@@ -79,8 +60,10 @@ watcher.on('create', function(file, stats) {
 
   var cmd = "create-torrent --urlList '" + URL_CHUNKS + beforeCompleteChunk + "' " + beforeCompleteChunk  + ' > ' + beforeCompleteChunkName + ".torrent";
   exec(cmd, {cwd:'/HLS/live/'} ,function(err, stdout, stderr){
-  if (err) {return console.log(err);}
-  console.log(stdout);
+    if (err) {return console.log(err);}
+    lastTorrent = URL_CHUNKS + beforeCompleteChunkName + ".torrent";
+    io.emit('new-chunk', lastTorrent);
+    console.log(stdout);
   });
 
 
@@ -91,3 +74,17 @@ watcher.on('change', function(file, stats) {
 watcher.on('delete', function(file) {
   console.log(file + ' was deleted')
 })
+
+
+//Cuando un usuario se conecta, mandarle el ultimo torrent generado.
+io.on('connection', function(socket){
+  socket.emit('play-stream', lastTorrent);
+  /*socket.on('new torrent', function(data){
+    console.log(data);
+    searchTorrents();
+  });
+  */
+});
+
+//Cada vez que se crea un torrent mandarlo a los clientes para que estos lo reproduzcan
+
